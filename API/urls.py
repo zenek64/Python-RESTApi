ffddc72380fe8894ceb8384deee0e9670afd90d0
grzegorz <150436@student.uwm.edu.pl>
2021-12-09 17:09:00 +0100

    serializers and views

--- API/urls.py
+++ API/urls.py
@@ -13,9 +13,22 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
 
+from jedzenie import views
+
+router = routers.DefaultRouter()
+router.register(r'restauracja', views.RestauracjaView)
+router.register(r'danie', views.DanieView)
+router.register(r'zamowienie', views.ZamowienieView)
+router.register(r'szczegoly', views.SzczegolyView)
 urlpatterns = [
-    path('admin/', admin.site.urls),
-]
+                  path('', include(router.urls)),
+                  path('admin/', admin.site.urls),
+                  path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(
+    settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- jedzenie/views.py
+++ jedzenie/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from jedzenie.models import Restauracja, Danie, Zamowienie, Szczegoly
+from jedzenie.serializers import RestauracjaSerializer, DanieSerializer, ZamowienieSerializer, SzczegolySerializer
+
+
+class RestauracjaView(viewsets.ModelViewSet):
+    queryset = Restauracja.objects.all()
+
+    serializer_class = RestauracjaSerializer
+
+class DanieView(viewsets.ModelViewSet):
+    queryset = Danie.objects.all()
+
+    serializer_class = DanieSerializer
+
+class ZamowienieView(viewsets.ModelViewSet):
+    queryset = Zamowienie.objects.all()
+
+    serializer_class = ZamowienieSerializer
+
+class SzczegolyView(viewsets.ModelViewSet):
+    queryset = Szczegoly.objects.all()
+
+    serializer_class = SzczegolySerializer
+
+
+
+
