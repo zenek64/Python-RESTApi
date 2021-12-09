7139772dbb199720f4920c425802bcc76f3bfb1a
grzegorz <150436@student.uwm.edu.pl>
2021-12-09 16:07:28 +0100

    models

--- jedzenie/admin.py
+++ jedzenie/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Restauracja
+from .models import Danie
+from .models import Zamowienie
+from .models import Szczegoly
 
 # Register your models here.
+admin.site.register(Restauracja)
+admin.site.register(Danie)
+admin.site.register(Zamowienie)
+admin.site.register(Szczegoly)
--- jedzenie/models.py
+++ jedzenie/models.py
@@ -1,3 +1,48 @@
 from django.db import models
 
-# Create your models here.
+
+
+class Restauracja(models.Model):
+    idRestauracja =models.AutoField(primary_key=True)
+    Nazwa = models.CharField(max_length=45)
+    Adres = models.CharField(max_length=45)
+    Rodzaj_dan = models.CharField(max_length=45)
+    Cena_dostawy = models.FloatField(max_length=12)
+    Kategoria= models.CharField(max_length=45)
+
+    def __str__(self):
+        return self.Nazwa
+
+
+class Danie(models.Model):
+    idDanie = models.AutoField(primary_key=True)
+    idRestauracja=models.ForeignKey(Restauracja,on_delete=models.CASCADE)
+    Nazwa=models.CharField(max_length=45)
+    Rodzaj=models.CharField(max_length=45)
+    Cena=models.FloatField(max_length=45)
+
+    def __str__(self):
+        return self.Nazwa
+
+
+
+
+
+
+class Zamowienie(models.Model):
+    idZamowienie=models.AutoField(primary_key=True)
+    Imie=models.CharField(max_length=45)
+    Nazwisko=models.CharField(max_length=45)
+    Adres=models.CharField(max_length=45)
+    Telefon=models.IntegerField()
+    Email= models.CharField(max_length=45)
+    Data=models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.Adres
+
+class Szczegoly(models.Model):
+    Ilosc= models.IntegerField()
+    Cena= models.FloatField(max_length=45)
+    Zamowienie_idZamowienie = models.ForeignKey(Zamowienie, on_delete=models.CASCADE)
+    Danie_idDanie = models.ForeignKey(Danie, on_delete=models.CASCADE)
